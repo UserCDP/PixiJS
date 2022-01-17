78b05f8b4481986f97e12fb4fcc97a50fa988902
UserCDP <cojocaru.daniela@icloud.com>
2022-01-17 16:46:51 +0200

    Drawn a square and a circle on the stage

--- src/project/Game.ts
+++ src/project/Game.ts
@@ -8,21 +8,8 @@ export class Game {
     }
 
     public startGame():void{
-        let cell:Graphics = new PIXI.Graphics();
-        cell.lineStyle(2, 0x000000, 0.5);
-        cell.beginFill(0xffffff);
-        cell.drawRect(100, 10, 100, 100);
-        cell.endFill();
-        //this.app.stage.addChild(cell);
-
-        //this.app.loader.add('monster','./assets/monster.png').load(this.onAsstsLoaded.bind(this));
-
-    }
-
-    private onAsstsLoaded(){
-        let monsterTexture = this.app.loader.resources['monster'].texture;
-        const monster = new PIXI.Sprite(monsterTexture);
-        this.app.stage.addChild(monster);
+        this.drawCircle();
+        this.drawSquare();
     }
 
     private drawSquare() {
@@ -32,6 +19,17 @@ export class Game {
         graphics.beginFill(0xDE3249);
         graphics.drawRect(50, 50, 100, 100);
         graphics.endFill();
-        //this.app.stage.addChild(graphics);
+        this.app.stage.addChild(graphics);
+    }
+
+    private drawCircle() {
+        let graphics:Graphics = new PIXI.Graphics();
+
+        // Circle
+        graphics.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
+        graphics.beginFill(0xDE3249, 1);
+        graphics.drawCircle(100, 250, 50);
+        graphics.endFill();
+        this.app.stage.addChild(graphics);
     }
 }
